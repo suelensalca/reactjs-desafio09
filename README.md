64a9f62c08c52b6a755d6658b329a5d830fbde8d
suelensalsa <suelen.leaf@gmail.com>
2022-03-04 17:14:34 -0300

    react-virtualized added

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1 align="center">Trilha ReactJS</h1>
+<h2>Jornada Ignite Rocketseat</h2>
+<div align="left">
+    <h3>Watchme - otimizando a aplicação</h3>
+    <br>
+    <p>Desafio realizado no Chapter 5 da Trilha ReactJS da Rocketseat.</p>
+    <p>A proposta do desafio foi refatorar uma aplicação de listagem de filmes para trabalhar os conceitos aprendidos no módulo. Com o objetivo de prover mais performance para a aplicação, usei as funcionalidades de memo, useCallback e virtualização com react-virtualized.
+</div>
+<h3>Screenshot</h3>
+<div align="center">
+    <img src="/public/watchme-landpage.png">
+</div>
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+import { List, ListRowRenderer } from 'react-virtualized';
 import { MovieCard } from "./MovieCard";
 
 interface ContentProps {
@@ -19,7 +21,20 @@ interface ContentProps {
   }>;
 }
 
-export function Content({ selectedGenre, movies }: ContentProps) {
+function ContentComponent({ selectedGenre, movies }: ContentProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <MovieCard
+          title={movies[index].Title}
+          poster={movies[index].Poster}
+          runtime={movies[index].Runtime}
+          rating={movies[index].Ratings[0].Value}
+        />
+      </div>
+    )
+  }
+
   return (
     <div className="container">
       <header>
@@ -27,12 +42,21 @@ export function Content({ selectedGenre, movies }: ContentProps) {
       </header>
 
       <main>
-        <div className="movies-list">
-          {movies.map(movie => (
-            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-          ))}
+        <div>
+          <List
+            className="movies-list"
+            height={520}
+            rowHeight={400}
+            width={800}
+            rowCount={movies.length}
+            rowRenderer={rowRenderer}
+          />
         </div>
       </main>
     </div>
   )
 }
+
+export const Content = memo(ContentComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.selectedGenre, nextProps.selectedGenre)
+})
